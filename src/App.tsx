b5d8e3f0dca5c62e5bd4b794e5f3dcd72643b4cc
Bikash Poudel <bikash.poudel@trussle.com>
2020-08-06 09:59:12 +0100

    var to const

--- src/App.tsx
+++ src/App.tsx
@@ -35,10 +35,10 @@ export const App: React.FC = () => {
         canvasContext?.drawImage(imgObj, 0, 0);
         const imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
          
-        for(var y = 0; y < imgPixels.height; y++){
-            for(var x = 0; x < imgPixels.width; x++){
-                var i = (y * 4) * imgPixels.width + x * 4;
-                var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+        for(const y = 0; y < imgPixels.height; y++){
+            for(const x = 0; x < imgPixels.width; x++){
+                const i = (y * 4) * imgPixels.width + x * 4;
+                const avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
                 imgPixels.data[i] = avg; 
                 imgPixels.data[i + 1] = avg; 
                 imgPixels.data[i + 2] = avg;
